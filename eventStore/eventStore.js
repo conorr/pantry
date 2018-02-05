da5ec2f82e424c949d1177af4c065204f45bdff2
Conor Ryan <ctryan3@gmail.com>
2018-02-05 11:13:28 -0800

    Fix linting errors

--- eventStore/eventStore.js
+++ eventStore/eventStore.js
@@ -1,13 +1,12 @@
 const mysql = require('mysql');
-const Joi = require('joi');
 const validateEvent = require('./validateEvent');
 const rowToEvent = require('./rowToEvent');
 
 const pool = mysql.createPool({
     connectionLimit: 5,
-    host     : 'localhost',
-    user     : 'root',
-    database : 'damoori'
+    host: 'localhost',
+    user: 'root',
+    database: 'damoori',
 });
 
 const getEvent = (sequenceId) => {
@@ -15,32 +14,36 @@ const getEvent = (sequenceId) => {
     return new Promise((resolve, reject) => {
         pool.getConnection((error, connection) => {
             if (error) reject(error);
-            connection.query(query, (error, result) => {
+            connection.query(query, (err, result) => {
                 connection.release();
-                if (error) reject(error);
-                if (result.length === 0) return;
-                const event = rowToEvent(result[0]);
+                if (err) reject(err);
+                let event;
+                if (result.length === 1) {
+                    event = rowToEvent(result[0]);
+                }
                 resolve(event);
             });
         });
     });
-}
+};
 
-const getEvents = (sequenceIdStart=0, top=100) => {
+const getEvents = (sequenceIdStart = 0, top = 100) => {
     const query = `select * from events where sequence_id >= ${sequenceIdStart} limit ${top}`;
     return new Promise((resolve, reject) => {
         pool.getConnection((error, connection) => {
             if (error) reject(error);
-            connection.query(query, (error, results) => {
+            connection.query(query, (err, results) => {
                 connection.release();
-                if (error) reject(error);
-                if (results.length === 0) return [];
-                const events = results.map(rowToEvent);
+                if (err) reject(err);
+                let events = [];
+                if (results.length > 0) {
+                    events = results.map(rowToEvent);
+                }
                 resolve(events);
             });
         });
     });
-}
+};
 
 const saveEvent = (event) => {
     validateEvent(event);
@@ -48,13 +51,15 @@ const saveEvent = (event) => {
     const query = `insert into events(type, version, body, created_utc) values ('${event.type}', ${event.version}, '${bodySerialized}', UTC_TIMESTAMP())`;
     pool.getConnection((error, connection) => {
         if (error) throw error;
-        connection.query(query, (error) => {
+        connection.query(query, (err) => {
             connection.release();
-            if (error) throw error;
+            if (err) throw err;
         });
-    })
-}
+    });
+};
 
-//saveEvent({ type: 'REMOVE_ITEM', version: 1, body: {foo: 'bar'}});
-//getEvent(1).then(event => console.log(event));
-getEvents().then(events => console.log(events));
+module.exports = {
+    getEvent,
+    getEvents,
+    saveEvent,
+};
--- eventStore/rowToEvent.js
+++ eventStore/rowToEvent.js
@@ -1,11 +1,9 @@
-const rowToEvent = (row) => {
-    return {
-        sequenceId: row['sequence_id'],
-        type: row['type'],
-        version: row['version'],
-        body: JSON.parse(row['body']),
-        createdUtc: row['created_utc'].toISOString()
-    };
-};
+const rowToEvent = row => ({
+    sequenceId: row.sequence_id,
+    type: row.type,
+    version: row.version,
+    body: JSON.parse(row.body),
+    createdUtc: row.created_utc.toISOString(),
+});
 
 module.exports = rowToEvent;
