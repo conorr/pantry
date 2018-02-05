7b79d5a0089f9f6513aed3315f118c4eaacc9a0b
Conor Ryan <ctryan3@gmail.com>
2018-02-04 21:35:32 -0800

    First commit of EventStore

---
+++ eventStore/eventStore.js
@@ -0,0 +1,69 @@
+const mysql = require('mysql');
+const Joi = require('joi');
+const validateEvent = require('./validateEvent');
+
+const pool = mysql.createPool({
+    connectionLimit: 5,
+    host     : 'localhost',
+    user     : 'root',
+    database : 'damoori'
+});
+
+const rowToEvent = (row) => {
+    return {
+        sequenceId: row['sequence_id'],
+        type: row['type'],
+        version: row['version'],
+        body: JSON.parse(row['body']),
+        createdUtc: row['created_utc'].toISOString()
+    };
+};
+
+const getEvent = (sequenceId) => {
+    const query = `select * from events where sequence_id = ${sequenceId}`;
+    return new Promise((resolve, reject) => {
+        pool.getConnection((error, connection) => {
+            if (error) reject(error);
+            connection.query(query, (error, result) => {
+                connection.release();
+                if (error) reject(error);
+                if (result.length === 0) return;
+                const event = rowToEvent(result[0]);
+                resolve(event);
+            });
+        });
+    });
+}
+
+const getEvents = (sequenceIdStart=0, top=100) => {
+    const query = `select * from events where sequence_id >= ${sequenceIdStart} limit ${top}`;
+    return new Promise((resolve, reject) => {
+        pool.getConnection((error, connection) => {
+            if (error) reject(error);
+            connection.query(query, (error, results) => {
+                connection.release();
+                if (error) reject(error);
+                if (results.length === 0) return [];
+                const events = results.map(rowToEvent);
+                resolve(events);
+            });
+        });
+    });
+}
+
+const saveEvent = (event) => {
+    validateEvent(event);
+    const bodySerialized = JSON.stringify(event.body);
+    const query = `insert into events(type, version, body, created_utc) values ('${event.type}', ${event.version}, '${bodySerialized}', UTC_TIMESTAMP())`;
+    pool.getConnection((error, connection) => {
+        if (error) throw error;
+        connection.query(query, (error) => {
+            connection.release();
+            if (error) throw error;
+        });
+    })
+}
+
+//saveEvent({ type: 'REMOVE_ITEM', version: 1, body: {foo: 'bar'}});
+//getEvent(1).then(event => console.log(event));
+getEvents().then(events => console.log(events));
---
+++ eventStore/validateEvent.js
@@ -0,0 +1,14 @@
+const Joi = require('joi');
+
+const schema = Joi.object().keys({
+    type: Joi.string().token().min(1).max(255).required(),
+    version: Joi.number().integer().greater(0),
+    body: Joi.object()
+});
+
+const validateEvent = (event) => {
+    const result = Joi.validate(event, schema);
+    if (result.error) throw new Error(`Event validation: ${result.error}`);
+}
+
+module.exports = validateEvent;
