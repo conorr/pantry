74dbea987488c074531fbb50b94309c197822928
Conor Ryan <ctryan3@gmail.com>
2018-02-05 11:36:41 -0800

    Fix bug

--- eventStore/validateEvent.js
+++ eventStore/validateEvent.js
@@ -4,7 +4,7 @@ const schema = Joi.object().keys({
     type: Joi.string().token().min(1).max(255)
         .required(),
     version: Joi.number().integer().greater(0),
-    namespace: Joi.string().max(255),
+    namespace: Joi.string().max(255).allow('').required(),
     body: Joi.object(),
 });
 
