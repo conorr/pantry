93a0cfd7bbdbe693365a3e6b9376c0d55754e227
Conor Ryan <ctryan3@gmail.com>
2018-09-04 13:09:51 -0700

    Add updatedUtc to report validator

--- src/reportCache/validateReport.js
+++ src/reportCache/validateReport.js
@@ -4,6 +4,7 @@ const schema = Joi.object().keys({
     cacheKey: Joi.string().min(1).max(255).required(),
     lastSequenceId: Joi.number().integer().required(),
     body: Joi.object().default(''),
+    updatedUtc: Joi.date(),
 });
 
 const validateReport = (report) => {
