bdb0d0037599a5b0cb74ac8224f478bd0a0515ab
Conor Ryan <ctryan3@gmail.com>
2018-09-04 16:22:43 -0700

    Refactored this about as much as I possibly could

--- src/reporting/reportBuilder.js
+++ src/reporting/reportBuilder.js
@@ -1,13 +1,3 @@
-const initializeReport = cacheKey => (
-    {
-        cacheKey,
-        // reminder: first event will have a sequenceId of 1, due to sqlite's
-        // autoincrement handling. https://sqlite.org/autoinc.html
-        lastSequenceId: 0,
-        body: {},
-    }
-);
-
 class ReportBuilder {
     constructor(eventStore, reportCache) {
         this.eventStore = eventStore;
@@ -17,24 +7,31 @@ class ReportBuilder {
     buildReport(cacheKey, reportBodyReducer) {
         return this.reportCache.getReport(cacheKey)
             .then((cachedReport) => {
-                const report = cachedReport || initializeReport(cacheKey);
+                const report = cachedReport ||
+                    ReportBuilder.initializeReport(cacheKey);
                 return this.eventStore.getEvents(report.lastSequenceId + 1)
-                    .then(events => (
-                        {
-                            report: report || initializeReport(cacheKey),
-                            events,
-                        }
-                    ));
+                    .then(events => ({ report, events }));
             })
             .then(({ report, events }) => {
                 if (events.length === 0) return Promise.resolve(report);
-                const lastSequenceId = events[events.length - 1].sequenceId;
-                const body = events.reduce(reportBodyReducer, report.body);
-                const newReport = { cacheKey, lastSequenceId, body };
-                return this.reportCache
-                    .saveReport(newReport)
+                const newReport = {
+                    cacheKey: report.cacheKey,
+                    lastSequenceId: events[events.length - 1].sequenceId,
+                    body: events.reduce(reportBodyReducer, report.body),
+                };
+                return this.reportCache.saveReport(newReport)
                     .then(() => newReport);
             });
+    }
+
+    static initializeReport(cacheKey) {
+        return {
+            cacheKey,
+            // Note: first event will have a sequenceId of 1, due to sqlite's
+            // autoincrement handling. https://sqlite.org/autoinc.html
+            lastSequenceId: 0,
+            body: {},
+        };
     }
 }
 
